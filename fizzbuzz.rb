cd7bc9307570fc1c41fedf1d52d573631e5e5b8b
Vince Andrew Pimentel <contact@vincep.me>
2018-10-03 06:04:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+fizz_3 = 3
+
+fizzbuzz(fizz_3)
+
 def fizzbuzz(number)
   if number % 3 == true
     "Fizz"
